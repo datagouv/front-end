8e541e03ad59202c40ebe7fa0b57e40813acacdc
Thibaud Dauce <git@hadibut.fr>
2025-03-04 16:52:01 +0100

    Fix list none in quality tooltip

--- components/dataset/QualityScore/QualityScoreTooltipContent/QualityScoreTooltipContent.vue
+++ components/dataset/QualityScore/QualityScoreTooltipContent/QualityScoreTooltipContent.vue
@@ -2,54 +2,56 @@
   <h5 class="fr-text--sm fr-my-0">
     {{ t("Metadata quality:") }}
   </h5>
-  <DatasetQualityItem
-    :passed="dataset.quality.dataset_description_quality"
-    :message-passed="t('Data description filled')"
-    :message-failed="t('Data description empty')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.resources_documentation"
-    :message-passed="t('Files documented')"
-    :message-failed="t('Files documentation missing')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.license"
-    :message-passed="t('License filled')"
-    :message-failed="t('No license set')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.update_frequency && !!dataset.quality.update_fulfilled_in_time"
-    :message-passed="t('Update frequency followed')"
-    :message-failed="dataset.quality.update_frequency ? t('Update frequency not followed') : t('Update frequency not set')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.has_open_format"
-    :message-passed="t('File formats are open')"
-    :message-failed="t('File formats are closed')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.temporal_coverage"
-    :message-passed="t('Temporal coverage filled')"
-    :message-failed="t('Temporal coverage not set')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.spatial"
-    :message-passed="t('Spatial coverage filled')"
-    :message-failed="t('Spatial coverage not set')"
-    class="fr-my-1w"
-  />
-  <DatasetQualityItem
-    :passed="dataset.quality.all_resources_available"
-    :message-passed="t('All files are available')"
-    :message-failed="t('Some files are unavailable')"
-    class="fr-my-1w"
-  />
+  <ul class="list-none pl-0">
+    <DatasetQualityItem
+      :passed="dataset.quality.dataset_description_quality"
+      :message-passed="t('Data description filled')"
+      :message-failed="t('Data description empty')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.resources_documentation"
+      :message-passed="t('Files documented')"
+      :message-failed="t('Files documentation missing')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.license"
+      :message-passed="t('License filled')"
+      :message-failed="t('No license set')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.update_frequency && !!dataset.quality.update_fulfilled_in_time"
+      :message-passed="t('Update frequency followed')"
+      :message-failed="dataset.quality.update_frequency ? t('Update frequency not followed') : t('Update frequency not set')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.has_open_format"
+      :message-passed="t('File formats are open')"
+      :message-failed="t('File formats are closed')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.temporal_coverage"
+      :message-passed="t('Temporal coverage filled')"
+      :message-failed="t('Temporal coverage not set')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.spatial"
+      :message-passed="t('Spatial coverage filled')"
+      :message-failed="t('Spatial coverage not set')"
+      class="fr-my-1w"
+    />
+    <DatasetQualityItem
+      :passed="dataset.quality.all_resources_available"
+      :message-passed="t('All files are available')"
+      :message-failed="t('Some files are unavailable')"
+      class="fr-my-1w"
+    />
+  </ul>
   <div class="fr-grid-row fr-grid-row--right not-enlarged">
     <a
       :href="'TODO'"
